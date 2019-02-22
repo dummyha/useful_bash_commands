ae63d620ebe09d9618f9c1118e5631495cb533d9
dummyha <47805095+dummyha@users.noreply.github.com>
2019-02-22 17:21:37 +0800

    Update one_liners.sh

--- one_liners.sh
+++ one_liners.sh
@@ -9,6 +9,6 @@ while read ip; do echo "$ip -       " `whois $ip | grep CIDR\|NetName`; done < I
 Cron: Every hour
 0 1 * * * /usr/bin/python /home/abc.py
 
-3) Finding Netstat along with WHOIS (time saving tricks) -
+*3) Finding Netstat along with WHOIS (time saving tricks)* -
 
 echo ""  && echo "[*] Finding connections..." && echo "" && (netstat -aptn 2>/dev/null | grep ESTABLISHED) && (echo "") && (echo "[*] Looking up WHOIS..Hang on...") && (echo "") && (netstat -aptn 2>/dev/null | cut -d ":" -f 2 | cut -d " " -f 6 | sed 's/established)//'| sed '/^$/d' | while IFS= read -r line; do echo "$line - `whois $line | grep OrgName`"; done | sed 's/OrgName://' | column -t)
